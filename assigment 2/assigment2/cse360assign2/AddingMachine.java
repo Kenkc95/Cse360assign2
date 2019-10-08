514258092a8d8343b675125ada96ec4838838099
kenkc <kenkc@DESKTOP-4SLTUSF.Home>
2019-10-08 13:25:31 -0700

    adding comment and URL address for grader

--- assigment 2/assigment2/cse360assign2/AddingMachine.java
+++ assigment 2/assigment2/cse360assign2/AddingMachine.java
@@ -1,5 +1,12 @@
 package cse360assign2;
-
+/*Tan Dao progrmaer 
+ * Professor Debra Callis 
+ * Octorber, 9, 2019
+ * Program help us to get used to github and posting program on qebsite
+ * here is the URL https://github.com/Kenkc95/Cse360assign2
+ * the class in the file assigment2 /cse360assign2 
+ * there are a main to test and class AddingMachine in packed cse360assign2
+ * */
 public class AddingMachine {
 	// instance variables
 	private int total;
